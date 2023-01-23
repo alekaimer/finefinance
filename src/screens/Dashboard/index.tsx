fe1bb823503487a81feb43a9c72a855f2e7bab03
Alexandre Kaimer <alekaimer@gmail.com>
2023-01-23 16:31:10 -0300

    feat: adds responsive fonts and Header component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container, Title } from './styles';
+import React from "react";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://github.com/alekaimer.png",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Alexandre</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,55 @@
-import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  flex-direction: row;
+  
+  background-color: ${({ theme }) => theme.colors.primary};
+
   justify-content: center;
   align-items: center;
+`;
 
-  background-color: ${({ theme }) => theme.colors.background};
-`
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+  margin-top: ${RFValue(28)}px;
+`;
 
-export const Title = styled.Text`
-  font-size: 24px;
+export const UserInfo = styled.View`
+  flex-direction: row;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  margin-right: 16px;
+  background-color: ${({ theme }) => theme.colors.shape};
+
+  border-radius: 10px;
+  border-width: 2px;
+  border-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const User = styled.View`
+  flex-direction: column;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
 `;
