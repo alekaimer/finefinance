3e7c8df3414e084ec05a856340c8b092f866fec8
Alexandre Kaimer <alekaimer@gmail.com>
2023-01-23 15:49:40 -0300

    feat: adds typescript, styled-components, global theme and Welcome and Dashboard components

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { Dashboard } from "./src/screens/Dashboard";
+
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/global/styles/theme";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
