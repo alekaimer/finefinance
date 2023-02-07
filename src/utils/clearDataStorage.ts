a543607c9d7fb9ae3a28465770b17f1a3f5b0cd8
Alexandre Kaimer <alekaimer@gmail.com>
2023-02-07 20:44:53 -0300

    refactor: clearDataStorage fn

--- src/utils/clearDataStorage.ts
+++ src/utils/clearDataStorage.ts
@@ -2,6 +2,5 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { DATA_KEY } from "../config/consts";
 
 export async function clearDataStorage() {
-  await AsyncStorage .removeItem(DATA_KEY);
-  const currentData = await AsyncStorage.getItem(DATA_KEY);
+  await AsyncStorage.removeItem(DATA_KEY);
 }
