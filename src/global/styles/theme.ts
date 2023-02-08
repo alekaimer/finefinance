920f1ef7b04f800e6b1fff8475225baf139f62eb
Alexandre Kaimer <alekaimer@gmail.com>
2023-02-07 23:06:51 -0300

    feat adds color in theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -11,6 +11,7 @@ export default {
     attention: '#E83F5B',
     attention_light: 'rgba(232, 63, 91, 0.5)',
 
+    black: '#000000',
     shape: '#FFFFFF',
     title: '#363F5F',
     text: '#969CB2',
