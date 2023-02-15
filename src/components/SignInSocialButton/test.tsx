f753fb7ce2a26460d375182262f1d133fdcf424f
Alexandre Kaimer <alekaimer@gmail.com>
2023-02-15 15:05:44 -0300

    refactor: SignInSocialButton svg prop in test

--- src/components/SignInSocialButton/test.tsx
+++ src/components/SignInSocialButton/test.tsx
@@ -60,8 +60,13 @@ describe("SignInSocialButton", () => {
         testID="sign-in-social-button"
         title="Entrar com login social"
         svg={() => (
-          <div
-          ></div>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          ></svg>
         )}
       />,
       { wrapper: Provider }
